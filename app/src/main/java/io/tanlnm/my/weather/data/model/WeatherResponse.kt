a5daf1421ec1fc94aef53e312d28d47d9e277cd0
Stanley <minhtan512@gmail.com>
2024-03-04 17:33:52 +0700

    Update data type

--- app/src/main/java/io/tanlnm/my/weather/data/model/WeatherResponse.kt
+++ app/src/main/java/io/tanlnm/my/weather/data/model/WeatherResponse.kt
@@ -1,9 +1,12 @@
 package io.tanlnm.my.weather.data.model
 
+import android.os.Parcelable
 import androidx.annotation.Keep
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
 @Keep
+@Parcelize
 data class Weather(
     val id: Long,
     val name: String,
@@ -21,26 +24,29 @@ data class Weather(
     // Base Params
     val cod: Int,
     val message: String?,
-) {
+) : Parcelable {
     val isSuccess: Boolean
         get() = cod in 200..299
 }
 
 @Keep
+@Parcelize
 data class Coordinator(
     val lat: Double,
     val lon: Double,
-)
+) : Parcelable
 
 @Keep
+@Parcelize
 data class WeatherInfo(
     val id: Int,
     val main: String,
     val description: String,
     val icon: String
-)
+) : Parcelable
 
 @Keep
+@Parcelize
 data class MainInfo(
     val temp: Double,
     @SerializedName("feels_like") val feelsLike: Double,
@@ -48,24 +54,27 @@ data class MainInfo(
     @SerializedName("temp_max") val tempMax: Double,
     val pressure: Double,
     val humidity: Double,
-)
+) : Parcelable
 
 @Keep
+@Parcelize
 data class SysInfo(
     val id: Long,
     val type: Int,
     val country: String,
     val sunrise: Long,
     val sunset: Long,
-)
+) : Parcelable
 
 @Keep
+@Parcelize
 data class WindInfo(
     val speed: Double,
     val deg: Int
-)
+) : Parcelable
 
 @Keep
+@Parcelize
 data class Clouds(
     val all: Int
-)
+) : Parcelable
